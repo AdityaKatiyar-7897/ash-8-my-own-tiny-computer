bf3f8565c3b03759486ba545d2d3d5dac083604e
Aditya Katiyar <rishkat1249@gmail.com>
2026-06-01 11:31:36 +0530

    computer just turned on

---
+++ ash8.c
@@ -0,0 +1,14 @@
+#include <SDL.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define COLS 40
+#define ROWS 25
+#define SCALE 16
+
+int main(void)
+{
+	printf("ASH-8 power on\n");
+	return 0;
+}

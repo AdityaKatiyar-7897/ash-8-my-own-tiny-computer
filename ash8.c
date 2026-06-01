6dafd8bdcd579b60889780ae12eb39606cb69949
Aditya Katiyar <rishkat1249@gmail.com>
2026-06-01 11:43:42 +0530

    We have a green square in the top-left cornet , that means screen memory -> monitor .. cool !

--- ash8.c
+++ ash8.c
@@ -9,11 +9,46 @@
 
 int main(void)
 {
-	uint8_t screen[COLS * ROWS] = {0};
+    uint8_t screen[COLS * ROWS] = {0};
 
-	screen[0] = 'A';
+    screen[0] = 'A';
 
-	printf("screen[0] contain: %c\n", screen[0]);
+    SDL_Init(SDL_INIT_VIDEO);
 
-	return 0 ;
+    SDL_Window *window = SDL_CreateWindow(
+        "ASH-8 Monitor",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        COLS * SCALE,
+        ROWS * SCALE,
+        0
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool running = true;
+    SDL_Event e;
+
+    while (running) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) running = false;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        if (screen[0] == 'A') {
+            SDL_Rect r = {0, 0, SCALE, SCALE};
+            SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+            SDL_RenderFillRect(renderer, &r);
+        }
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
